c4c884c537800590e7eb576ea723bfb171d30795
Meksich <maks10031003@gmail.com>
2021-03-29 17:08:31 +0300

    Made lab-5 with regex

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import re
+
+
 def main():
-    START_TIME = b"[01/Jul/1995:00:10:"
-    END_TIME = b"[01/Jul/1995:00:20:"
-    KEY_PHRASE = b"NASA"
     is_needed_time = False
     line_counter = 0
-
-    with open("access_log_Jul95", 'rb') as a:
-        file = a.readlines()
+    file = open("access_log_Jul95")
 
     for line in file:
-        if END_TIME in line:
+        if re.search(r"01/Jul/1995:00:20:\d\d", line) is not None:
             is_needed_time = False
             break
-        if START_TIME in line:
+        if re.search(r"01/Jul/1995:00:10:\d\d", line) is not None:
             is_needed_time = True
-        if is_needed_time and KEY_PHRASE in line:
+        if is_needed_time and re.search(r"NASA", line) is not None:
             line_counter = line_counter + 1
 
     print(line_counter)
